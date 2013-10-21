f4ede5095d9bebb28bc22bc4b6f044f1c7f37d20
Zeex <zeex@rocketmail.com>
2013-10-21 23:22:35 +0700

    SAMP -> SA-MP

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char **argv) {
 
   QCoreApplication::setApplicationName("qawno");
   QCoreApplication::setApplicationVersion("1.0");
-  QCoreApplication::setOrganizationName("SAMP");
+  QCoreApplication::setOrganizationName("SA-MP");
 
   MainWindow mainWindow;
   mainWindow.show();
