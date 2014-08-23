803d6aba40a0f80bd23c23d93f016a24933cbe54
Zeex <zeex@rocketmail.com>
2014-08-23 16:50:49 +0700

    Set organization to "SAMP"

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char **argv) {
 
   QCoreApplication::setApplicationName("qawno");
   QCoreApplication::setApplicationVersion("1.0");
-  QCoreApplication::setOrganizationName("qawno");
+  QCoreApplication::setOrganizationName("SAMP");
 
   MainWindow mainWindow;
   mainWindow.show();
