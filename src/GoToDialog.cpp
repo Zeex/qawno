8aef3bcbef7f48c5d2f8ed98c178062713f19980
Zeex <zeex@rocketmail.com>
2014-08-26 03:11:27 +0700

    Add license header
    Apparently in order to statically link with Qt you have to license
    your code under GPL. So here we go...

--- src/GoToDialog.cpp
+++ src/GoToDialog.cpp
@@ -1,3 +1,18 @@
+// This file is part of qawno.
+//
+// qawno is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// qawno is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with qawno.  If not, see <http://www.gnu.org/licenses/>.
+
 #include "GoToDialog.h"
 #include "ui_GoToDialog.h"
 
